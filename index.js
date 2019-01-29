075acf180be3216e24dfe1e2c3a7e4d588e158c4
Vladimir Savrov <v.savrov@aristos.pw>
2019-01-29 15:03:40 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const request = require('request');
+const cheerio = require('cheerio');
+
+const outputPath = 'output';
+
+if (!fs.existsSync(outputPath)) {
+  fs.mkdirSync(outputPath);
+}
+
+let entryPoint = 'https://adamimages.sbdinc.com/GEM/Stanley/1000x1000_144r/';
+let baseUrl = 'https://adamimages.sbdinc.com';
+let currentItem = 0;
+
+request(entryPoint, (error, response, body) => {
+  if (!error) {
+    let $ = cheerio.load(body);
+    let images = $('a[href$=jpg]');
+
+    console.log('Count: ' + images.length);
+
+    serialDownload(images, $);
+  }
+});
+
+const waitFor = (ms) => new Promise(r => setTimeout(r, ms));
+
+let serialDownload = (images, $) => {
+  let imagePath = outputPath + '/' + $(images[currentItem]).text();
+
+  if (fs.existsSync(imagePath)) {
+    currentItem++;
+
+    serialDownload(images, $);
+  } else {
+    download(baseUrl + $(images[currentItem]).attr('href'), imagePath, async () => {
+      await waitFor(50);
+      console.log($(images[currentItem]).text() + ' - done');
+
+      if (images.length > currentItem) {
+        currentItem++;
+
+        serialDownload(images, $);
+      } else {
+        console.log('Done');
+      }
+    });
+  }
+};
+
+let download = (uri, filename, callback) => {
+  if (!callback) {
+    callback = () => {
+    };
+  }
+
+  request.head(uri, (err, res, body) => {
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
